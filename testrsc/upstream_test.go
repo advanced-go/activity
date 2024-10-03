f731623d41038f7100e73210ef04f03a38d4e203
advanced-go <mark.wilson.go@outlook.com>
2024-10-03 10:41:38 -0500

    Refactoring

--- testrsc/upstream_test.go
+++ testrsc/upstream_test.go
@@ -10,11 +10,24 @@ import (
 )
 
 const (
-	dirPath = "file://[cwd]/files/upstream"
+	eventsDir   = "file://[cwd]/files/upstream/events"
+	customerDir = "file://[cwd]/files/upstream/customer"
 )
 
-func path(name string) string {
-	return dirPath + "/" + name
+type exchange struct {
+	dir, req, resp string
+}
+
+func requestPath(ex exchange) string {
+	return ex.dir + "/" + ex.req
+}
+
+func responsePath(ex exchange) string {
+	if ex.resp != "" {
+		return ex.dir + "/" + ex.resp
+	}
+	s := strings.Replace(ex.req, ".", "-resp.", 1)
+	return ex.dir + "/" + s
 }
 
 func newUrl(req *http.Request) string {
@@ -36,44 +49,48 @@ func newRequest(req *http.Request) (*http.Request, error) {
 }
 
 func ExampleBuildUpstream() {
-	items := []struct {
-		req  string
-		resp string
-	}{
-		{req: "customer-get-req.txt", resp: "customer-get-resp.txt"},
+	items := []exchange{
+		{dir: customerDir, req: "get-req.txt", resp: ""},
+		{dir: customerDir, req: "get-all-req.txt", resp: ""},
+
+		{dir: eventsDir, req: "log-egress-v1-get-all-req.txt", resp: ""},
+		{dir: eventsDir, req: "log-egress-v2-get-all-req.txt", resp: ""},
+		{dir: eventsDir, req: "log-ingress-v1-get-all-req.txt", resp: ""},
+		{dir: eventsDir, req: "log-ingress-v2-get-all-req.txt", resp: ""},
 
 		//
 	}
+
 	fmt.Printf("BuildUpstream() - start\n")
 
 	for _, i := range items {
 		var err error
 
-		req, status := httpxtest.NewRequest(path(i.req))
+		req, status := httpxtest.NewRequest(requestPath(i))
 		if !status.OK() {
-			fmt.Printf("error: NewRequest() ->  %v\n", status)
+			fmt.Printf("error: NewRequest(\"%v\") ->  %v\n", i.req, status)
 			continue
 		}
 		req, err = newRequest(req)
 		if err != nil {
-			fmt.Printf("error: updateUrl() -> %v\n", err)
+			fmt.Printf("error: updateUrl(\"%v\") -> %v\n", i.req, err)
 			continue
 		}
 		resp, status1 := httpx.Do(req)
 		if !status1.OK() {
-			fmt.Printf("error: Do() -> %v\n", status)
+			fmt.Printf("error: Do(\"%v\") -> %v\n", i.req, status1)
 			continue
 		}
 		if resp.Header.Get(httpx.ContentType) == httpx.ContentTypeJson {
 			resp.Body, status = json2.Indent(resp.Body, resp.Header, "", "  ")
 			if !status.OK() {
-				fmt.Printf("error: json2.Indent() -> %v\n", status)
+				fmt.Printf("error: json2.Indent(\"%v\") -> %v\n", i.req, status)
 				continue
 			}
 		}
-		status = httpxtest.WriteResponse(path(i.resp), resp)
+		status = httpxtest.WriteResponse(responsePath(i), resp)
 		if !status.OK() {
-			fmt.Printf("error: WriteResponse() -> %v\n", status)
+			fmt.Printf("error: WriteResponse(\"%v\") -> %v\n", i.req, status)
 			continue
 		}
 	}
@@ -85,3 +102,14 @@ func ExampleBuildUpstream() {
 	//BuildUpstream() - stop
 
 }
+
+/*
+func ExampleBuildUpstream() {
+	items := []exchange {{
+		req  string
+		resp string
+	},
+	}{
+
+
+*/

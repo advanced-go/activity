7d4e4173974427b8bb779830e5621e6de22ee297
advanced-go <mark.wilson.go@outlook.com>
2024-10-14 12:08:01 -0500

    Update get.go

--- customer1/get.go
+++ customer1/get.go
@@ -2,25 +2,15 @@ package customer1
 
 import (
 	"context"
-	"github.com/advanced-go/activity/testrsc"
 	"github.com/advanced-go/stdlib/core"
 	"github.com/advanced-go/stdlib/httpx"
-	"github.com/advanced-go/stdlib/uri"
 	"net/http"
 	"net/url"
 )
 
 const (
 	customerKey = "customer"
-	stateKey    = "state"
 )
-
-func testOverride(h http.Header) http.Header {
-	if h != nil && h.Get(uri.XContentResolver) != "" {
-		return h
-	}
-	return httpx.SetHeader(h, uri.XContentResolver, testrsc.CustomerV1Entry)
-}
 
 func get[E core.ErrorHandler](ctx context.Context, h http.Header, resource string, values url.Values) (entries []Entry, h2 http.Header, status *core.Status) {
 	var e E
@@ -29,9 +19,6 @@ func get[E core.ErrorHandler](ctx context.Context, h http.Header, resource strin
 	if values == nil || len(values) == 0 {
 		return nil, h2, core.StatusNotFound()
 	}
-	// Test only
-	//h = testOverride(h)
-
 	// Build requests
 	reqs, status1 := buildRequests(ctx, h, resource, values)
 	if !status1.OK() {
@@ -55,9 +42,6 @@ func get[E core.ErrorHandler](ctx context.Context, h http.Header, resource strin
 		e.Handle(status.WithRequestId(h))
 		return
 	}
-
-	// Test only
-	//entries = filter(entries, values)
 	if len(entries) == 0 {
 		status = core.NewStatus(http.StatusNotFound)
 	} else {
@@ -65,23 +49,4 @@ func get[E core.ErrorHandler](ctx context.Context, h http.Header, resource strin
 		status = core.StatusOK()
 	}
 	return
-}
-
-func filter(entries []Entry, values url.Values) (result []Entry) {
-	customer := values.Get(customerKey)
-	state := values.Get(stateKey)
-	for _, e := range entries {
-		if customer == "*" {
-			result = append(result, e)
-			continue
-		}
-		if customer != "" && customer != e.CustomerId() {
-			continue
-		}
-		if state != "" && state != e.State() {
-			continue
-		}
-		result = append(result, e)
-	}
-	return result
 }
